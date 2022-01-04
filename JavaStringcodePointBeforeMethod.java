339f6d6db772ccd8f57ba079e0f4d27a25f88b74
codingwith suraj <kshatriyasuraj00@gmail.com>
2022-01-04 11:46:09 +0530

    Java String codePointBefore() Method :

--- JavaStringcodePointBeforeMethod.java
+++ JavaStringcodePointBeforeMethod.java
@@ -1,4 +1,15 @@
 public class JavaStringcodePointBeforeMethod {
     
-    
+    public static void main(String[] args){
+        // Java String  codePointBefore() Method : 
+        //Syntax : public int codePointBefore(int index)
+        //it returns the unicode value of the character before the specified index or a string otherwise it will throw 
+        //IndexOutOfBoundsException -if index is negative or not less than the length of the specified string.
+
+        String str= "hello";
+        int result = str.codePointBefore(2);
+        int result1 = str.codePointBefore(1);
+        System.out.println("The unicode of e and h are "+ result + "and "+ result1);
+
+    }
 }
